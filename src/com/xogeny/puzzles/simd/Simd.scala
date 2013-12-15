556ee8434a4f7fe9c8387dcef33d0fe98d010104
Michael Tiller <michael.tiller@gmail.com>
2013-12-14 21:26:37 -0500

    Generating constraints
    This approach seems like it is really efficient.  By
    presupposing the solution, we can reduce the number
    of constraints significantly.

--- src/com/xogeny/puzzles/simd/Simd.scala
+++ src/com/xogeny/puzzles/simd/Simd.scala
@@ -44,9 +44,9 @@ case class Space(color: Color, number: Int, path: Set[Color], x: Int, y: Int) {
   val name: String = "s("+x+","+y+")";
 }
 
-case class Board(val spaces: List[Space]);
+case class Board(spaces: List[Space]);
 
-case class Problem(val board: Board, ballNames: List[String]) {
+case class Problem(board: Board, ballNames: List[String]) {
   val store = new Store();
   val balls: Map[String, IntVar] = Map() ++ (ballNames map { n =>
     val v = new IntVar(store, n, 0, board.spaces.length-1)
@@ -54,7 +54,8 @@ case class Problem(val board: Board, ballNames: List[String]) {
   });
   val vars: List[IntVar] = balls.values.toList
   store.impose(new Alldifferent(vars.toArray))
-  def impose(c: SimdConstraint) = c.constraints map { store.impose(_) };
+  def impose(c: SimdConstraint): Unit = c.constraints(this) foreach { store.impose(_) };
+  def impose(cl: List[SimdConstraint]): Unit = cl foreach { impose(_) }
   def ball(n: String) = balls.get(n).get;
 
   def solve() = {
@@ -73,52 +74,85 @@ case class Problem(val board: Board, ballNames: List[String]) {
   }
 }
 
-abstract class SimdConstraint(prob: Problem) {
-  def constraints: List[Constraint];
+trait ConstraintGenerator {
+  def allValid(board: Board, sol: Map[String, Int]): List[SimdConstraint];
 }
 
-abstract class PrimaryConstraint(prob: Problem, ball: String) extends SimdConstraint(prob) {
-  def satisfies(s: Space): Boolean;
-  def constraints: List[Constraint] = {
-    val candidates = ((0 to (prob.board.spaces.length-1)).toList filter { s => satisfies(prob.board.spaces(s)) }).toArray
-    val eqs = candidates map { new XeqC(prob.ball(ball), _) }
-    var ret = new util.ArrayList[PrimitiveConstraint];
-    eqs foreach { ret.add(_) }
-    List(new Or(ret));
+object SimdConstraint extends ConstraintGenerator {
+  def allValid(board: Board, sol: Map[String,Int]): List[SimdConstraint] = {
+    IsNumber.allValid(board, sol) ::: IsColor.allValid(board, sol) ::: SameColor.allValid(board, sol)
   }
 }
 
-case object IsNumber {
-  def matching(v: String, s: Space, prob: Problem): List[IsNumber] = List(IsNumber(v, s.number, prob))
+abstract class SimdConstraint {
+  def constraints(prob: Problem): List[Constraint];
 }
-case class IsNumber(v: String, n: Int, prob: Problem) extends PrimaryConstraint(prob, v) {
+
+abstract class PrimaryConstraint(ball: String) extends SimdConstraint {
+  def satisfies(s: Space): Boolean;
+  def constraints(prob: Problem): List[Constraint] = {
+    val candidates = ((0 to (prob.board.spaces.length-1)).toList filter { s => satisfies(prob.board.spaces(s)) }).toArray
+    val eqs = candidates map { new XeqC(prob.ball(ball), _) }
+    if (eqs.length==1) List(eqs(0))
+    else {
+      var ret = new util.ArrayList[PrimitiveConstraint];
+      eqs foreach { ret.add(_) }
+      List(new Or(ret));
+    }
+  }
+}
+
+case object IsNumber extends ConstraintGenerator {
+  def allValid(board: Board, sol: Map[String, Int]): List[SimdConstraint] = {
+    sol.toList map { p => IsNumber(p._1, board.spaces(p._2).number) }
+  }
+}
+case class IsNumber(v: String, n: Int) extends PrimaryConstraint(v) {
   def satisfies(s: Space) = s.number==n;
 }
 
-case object IsColor {
-  def matching(v: String, s: Space, prob: Problem): List[IsColor] = List(IsColor(v, s.color, prob))
+case object IsColor extends ConstraintGenerator {
+  def allValid(board: Board, sol: Map[String, Int]): List[SimdConstraint] = {
+    sol.toList map { p => IsColor(p._1, board.spaces(p._2).color) }
+  }
 }
-case class IsColor(v: String, c: Color, prob: Problem) extends PrimaryConstraint(prob, v) {
+case class IsColor(v: String, c: Color) extends PrimaryConstraint(v) {
   def satisfies(s: Space) = s.color==c;
 }
 
-abstract class SecondaryConstraint(b1: String, b2: String, prob: Problem) extends SimdConstraint(prob) {
-  def constraints: List[Constraint] = {
-    val spaces = (0 to (prob.board.spaces.length-1));
+abstract class SecondaryConstraint(b1: String, b2: String) extends SimdConstraint {
+  def constraints(prob: Problem): List[Constraint] = {
+    val spaces = 0 to (prob.board.spaces.length-1);
     val candidates = for(i <- spaces;
                          j <- spaces;
                          if i!=j;
                          si <- Some(prob.board.spaces(i));
                          sj <- Some(prob.board.spaces(j));
                          if satisfies(si,sj)) yield new And(new XeqC(prob.ball(b1), i), new XeqC(prob.ball(b2), j))
-    var ret = new util.ArrayList[PrimitiveConstraint];
-    candidates foreach { ret.add(_) }
-    List(new Or(ret));
+    if (candidates.length==1) List(candidates(0))
+    else {
+      var ret = new util.ArrayList[PrimitiveConstraint];
+      candidates foreach { ret.add(_) }
+      List(new Or(ret));
+    }
   }
   def satisfies(s1: Space, s2: Space): Boolean;
 }
 
-case class SameColor(b1: String, b2: String, prob: Problem) extends SecondaryConstraint(b1, b2, prob) {
+case object SameColor extends ConstraintGenerator {
+  def allValid(board: Board, sol: Map[String, Int]): List[SimdConstraint] = {
+    val names = sol.keys.toList
+    val valid = for(i <- names;
+                    j <- names;
+                    si <- sol.get(i) map { board.spaces(_) };
+                    sj <- sol.get(j) map { board.spaces(_) };
+                    if i!=j && si.color==sj.color) yield SameColor(i, j)
+    println("Valid = "+valid)
+    valid
+  }
+}
+
+case class SameColor(b1: String, b2: String) extends SecondaryConstraint(b1, b2) {
   def satisfies(s1: Space, s2: Space) = s1.color==s2.color;
 }
 
@@ -131,6 +165,19 @@ class Simd(r: Int, c: Int) {
 
 }
 
+class ProblemGenerator(board: Board, root: String, other: List[String], diff: Int=1) {
+  def solve: List[SimdConstraint] = other match {
+    case Nil => solveRootOnly
+    case y => {
+      val cons = y flatMap { n => new ProblemGenerator(board, n, Nil, diff).solve }
+      cons
+    }
+  }
+  def solveRootOnly: List[SimdConstraint] = {
+    Nil
+  }
+}
+
 object TestSimd {
   def main(args: Array[String]): Unit = {
     simpleSolve();
@@ -138,11 +185,10 @@ object TestSimd {
 
   def simpleSolve() = {
     val board = Board(List(Space(Black, 1, Set(), 0, 0), Space(Black, 2, Set(), 0, 0)))
+    val pos = SimdConstraint.allValid(board, Map("alpha" -> 0, "beta" -> 1))
+    println("Possible constraints: "+pos);
     val prob = Problem(board, List("alpha", "beta"))
-    val c1 = IsNumber("alpha", 1, prob);
-    val c2 = SameColor("alpha", "beta", prob);
-    println("constraits: "+c1.constraints);
-    prob.impose(c1);
+    prob.impose(pos);
     println(prob.store);
     prob.solve();
   }
