3d54b8a128bfe3f9de4fb8fa2b86797f6df033c8
Michael Tiller <michael.tiller@gmail.com>
2013-12-25 09:10:17 -0500

    Random plan generation and flexible problem statement

--- src/com/xogeny/puzzles/simd/Simd.scala
+++ src/com/xogeny/puzzles/simd/Simd.scala
@@ -10,12 +10,12 @@ object TestSimd {
   def main(args: Array[String]): Unit = {
     //simpleSolve();
     //generateProblem()
-    (0 to 2) map { sculptProblem(_, false) }
+    (0 to 2) map { sculptProblem(_, 4, 4, false) }
   }
 
-  def sculptProblem(seed0: Int, verbose: Boolean) = {
+  def sculptProblem(seed0: Int, sz: Int, nvars: Int, verbose: Boolean) = {
     var seed = seed0;
-    var ret = sculptPossibleProblem(seed, 6, verbose)
+    var ret = sculptPossibleProblem(seed, sz, nvars, verbose)
     if (ret==None) {
       println("Invalid problem")
     } else {
@@ -28,17 +28,15 @@ object TestSimd {
     }
   }
 
-  def sculptPossibleProblem(seed: Int, nvars: Int, verbose: Boolean): Option[(Board, Map[String,Int], List[SimdConstraint])] = {
-    val board = Board.random(5, 5, seed, 5, List(Red, Green, Blue, Yellow, Purple))
-    //val plan = DTree(List("X" -> "A", "X" -> "B", "A" -> "C", "B" -> "D"))
-    //val plan = DTree(List("X" -> "A", "X" -> "B"))
+  def sculptPossibleProblem(seed: Int, sz: Int, nvars: Int, verbose: Boolean): Option[(Board, Map[String,Int], List[SimdConstraint])] = {
+    val colors = List(Red, Green, Blue, Yellow, Purple, Cyan)
+    val board = Board.random(sz, sz, seed, sz, colors take sz)
     val sol = Board.randomSolution(board, nvars);
     println("Random board: "+board)
     println("Random solution: "+sol);
     val valid = SimdConstraint.allValid(board, sol)
-    //val tweaker = NullTweaker
-    val plan = List("A" -> "B", "A" -> "C", "C" -> "D", "C" -> "X", "B" -> "E")
-    //val plan = List("X" -> "A", "X" -> "B", "B" -> "C")
+    val plan = TreeTweaker.randomPlan(sol.keys.toList)
+    println("Plan = "+plan)
     val tweaker = TreeTweaker(plan)
     val gen = Sculptor(board, sol, valid, tweaker, verbose)
     val cons = gen.solve(sol.keys.toList)
--- src/com/xogeny/puzzles/simd/Tweaker.scala
+++ src/com/xogeny/puzzles/simd/Tweaker.scala
@@ -1,5 +1,7 @@
 package com.xogeny.puzzles.simd
 
+import scala.util.Random
+
 /**
  * Created by mtiller on 12/24/13.
  */
@@ -12,6 +14,31 @@ case object NullTweaker extends Tweaker {
   def adjustPriority(c: SimdConstraint): Int = 0;
 }
 
+case object TreeTweaker {
+  private def process(vars: List[String]): List[(String,String)] = vars match {
+    case x :: a :: b :: r => {  /* x depends on a and b */
+      val head = List(x -> a, x -> b)
+      r match {
+        case Nil => (x -> a) :: (x -> b) :: Nil
+        case _ => {
+          val tlen = r length
+          val n = Random.nextInt(tlen)
+          val atail = r take n
+          val btail = r drop n
+          head ::: process(a :: atail) ::: process(b :: btail)
+        }
+      }
+    }
+    case x :: a :: Nil => {  /* x depends only on a */
+      (x, a) :: Nil
+    }
+    case x :: Nil => Nil  /* x is a leaf */
+    case Nil => Nil /* done */
+  }
+  def randomPlan(vars: List[String]) = {
+    process(Random.shuffle(vars));
+  }
+}
 case class TreeTweaker(plan: List[Pair[String,String]]) extends Tweaker {
   def isLeaf(s: String) = plan forall { p => p._1 != s }
   def parent(s: String) = plan filter { p => p._2==s } map { _._1 }
