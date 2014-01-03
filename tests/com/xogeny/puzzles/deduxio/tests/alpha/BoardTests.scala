12ce7af99f69bfa536bdf57ff7b05678a92ff9da
Michael Tiller <michael.tiller@gmail.com>
2014-01-03 08:40:29 -0500

    Adding some tests (and already getting a failure)

--- tests/com/xogeny/puzzles/deduxio/tests/alpha/BoardTests.scala
+++ tests/com/xogeny/puzzles/deduxio/tests/alpha/BoardTests.scala
@@ -1,8 +1,15 @@
 package com.xogeny.puzzles.deduxio.tests.alpha
 
+import org.scalatest.FunSuite
+import com.xogeny.puzzles.deduxio.alpha.repr._
+
 /**
  * Created by mtiller on 1/3/14.
  */
-class BoardTests {
-
+class BoardTests extends FunSuite {
+  test("Spaces in B1") {
+    assert(Samples.B1.spaces(0).number==1);
+    assert(Samples.B1.paths(Samples.B1.spaces(0))==Set(Blue))
+    assert(Samples.B1.adjacent(4)==Set(1,3,5,7))
+  }
 }
--- tests/com/xogeny/puzzles/deduxio/tests/alpha/Samples.scala
+++ tests/com/xogeny/puzzles/deduxio/tests/alpha/Samples.scala
@@ -6,7 +6,9 @@ import com.xogeny.puzzles.deduxio.alpha.repr._
  * Created by mtiller on 1/3/14.
  */
 
-case object P1 extends Paths(Map(Blue -> List(1, 2, 3), Green -> List(4, 5, 6), Blue -> List(7, 8, 9)));
-case object B1 extends Board(List(Space(Red, 1, 1, 1), Space(Red, 2, 2, 1), Space(Red, 3, 3, 1),
-                                  Space(Blue, 1, 1, 2), Space(Blue, 2, 2, 2), Space(Blue, 3, 3, 2),
-                                  Space(Green, 1, 1, 3), Space(Green, 2, 2, 3), Space(Green, 3, 3, 3)), P1);
+object Samples {
+  val P1 = Paths(Map(Blue -> List(1, 2, 3), Green -> List(4, 5, 6), Blue -> List(7, 8, 9)));
+  val B1 = Board(List(Space(Red,   1, 1, 1), Space(Red,   2, 2, 1), Space(Red,   3, 3, 1),
+                      Space(Blue,  1, 1, 2), Space(Blue,  2, 2, 2), Space(Blue,  3, 3, 2),
+                      Space(Green, 1, 1, 3), Space(Green, 2, 2, 3), Space(Green, 3, 3, 3)), P1);
+}
